9906cf355953252a7a2d762052aa5820c62398f4
sindoring <592211064Aa>
2020-02-07 00:56:12 +0400

    second

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -15,7 +15,9 @@ class Authenticate extends Middleware
     protected function redirectTo($request)
     {
         if (! $request->expectsJson()) {
-            return route('login');
+            abort(response()->json([
+                'message'=>'token is not provided'
+            ],403));
         }
     }
 }
